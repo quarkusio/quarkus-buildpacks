965f19b5daa2c7589df05c447467833b478f0bcb
Tako Schotanus <tako@codejive.org>
2021-01-07 18:36:32 +0100

    fix: Minor script fixes

--- create-buildpacks.sh
+++ create-buildpacks.sh
@@ -12,17 +12,18 @@ STACKS_DIR=$DIR/stacks
 BUILDERS_DIR=$DIR/builders
 BUILDPACKS_DIR=$DIR/buildpacks
 
-echo ---> Building Stacks
+echo "---> Building Stacks"
 
-for stack_dir in $(find ${STACKS_DIR} -type d -maxdepth 1 -mindepth 1)
+for stack_dir in $(find ${STACKS_DIR} -maxdepth 1 -mindepth 1 -type d)
 do
-  echo ---> Building Stack $(basename ${stack_dir})
-  $DIR/build-stack.sh -p ${REPO_PREFIX} ${stack_dir}
+  echo "---> Building Stack $(basename ${stack_dir})"
+  $DIR/stacks/build-stack.sh -p ${REPO_PREFIX} ${stack_dir}
 done
 
-for builder_dir in $(find ${BUILDERS_DIR} -type d -maxdepth 1 -mindepth 1)
+for builder_dir in $(find ${BUILDERS_DIR} -maxdepth 1 -mindepth 1 -type d)
 do
-  echo ---> Creating builder for builder $(basename ${builder_dir})
+  echo "---> Creating builder for builder $(basename ${builder_dir})"
   pack create-builder redhat/$(basename ${builder_dir}):latest --config ${builder_dir}/builder.toml
   pack trust-builder redhat/$(basename ${builder_dir}):latest
 done
+
