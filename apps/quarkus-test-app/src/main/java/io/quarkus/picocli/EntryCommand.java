f5b673c94a05a54f979944ddd0d474a5d8b4f25c
Tako Schotanus <tako@codejive.org>
2021-04-07 16:43:36 +0200

    ci: Added test app for CI

---
+++ apps/quarkus-test-app/src/main/java/io/quarkus/picocli/EntryCommand.java
@@ -0,0 +1,9 @@
+package io.quarkus.picocli;
+
+import io.quarkus.picocli.runtime.annotations.TopCommand;
+import picocli.CommandLine;
+
+@TopCommand
+@CommandLine.Command(mixinStandardHelpOptions = true, subcommands = { HelloCommand.class, GoodbyeCommand.class })
+public class EntryCommand {
+}
